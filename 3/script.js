1627aaebb80d3ef83fd0608d65017f2273a7950d
Syukirman Amir <128995740+syukirman-amir@users.noreply.github.com>
2025-05-09 16:06:55 +0900

    Update script.js

--- 3/script.js
+++ 3/script.js
@@ -5,7 +5,6 @@ function handleSubmit(event) {
     const outputDiv = document.getElementById('output');
 
     // Validasi URL dengan regex: protokol apa pun, diikuti ://, lalu domain
-    const urlPattern = /^[a-zA-Z0-9+.-]+:\/\/[^\s/$.?#].[^\s]*$/i;
     const urlPattern = /^[a-zA-Z0-9+.-]+:\/\/(?!.*(%0a|%0d)).+$/i;
     if (!urlPattern.test(urlInput)) {
         outputDiv.innerHTML = '<p style="color: red;">Error: Please enter a valid URL with protocol (e.g., http://example.com).</p>';
